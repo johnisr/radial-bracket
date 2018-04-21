3ccfc874d5ef9a12892c2f7bbf1401577cfc828b
John <john.i.s.ramos@gmail.com>
2018-04-21 01:47:06 -0400

    Add imaage Svg to RadialBracketPie

--- src/components/RadialBracketPage/RadialBracket/RadialBracketPie/RadialBracketPie.js
+++ src/components/RadialBracketPage/RadialBracket/RadialBracketPie/RadialBracketPie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { arc, pie } from 'd3-shape';
 import nbaColours from '../../../../data/nbaColours';
+import nbaLogos from '../../../../data/nbaLogos';
 
 const RadialBracketPie = (props) => {
   // Props
@@ -9,17 +10,28 @@ const RadialBracketPie = (props) => {
   const round = props.round;
   const dimensions = props.data.dimensions;
   const margin = props.data.margin;
+  const showImages = props.data.showImages;
 
   // Derived From Props
   const level = Math.log2(round.length);
   const width = dimensions[0] - margin.left - margin.right;
 
-  // Settings (want to eventually base of Props)
+  // Settings (want to eventually base of Props) -------------------------
   // Path Settings
   const inChartMarginAngle = 0.02;
   const marginAngleLeveller = (level) => level * (inChartMarginAngle / 10);
 
-  // Functions to calculate SVGs
+  //Text Settings
+  const textY = '.37em';
+
+  // Image Settings
+  const imageMargin = 10;
+  const centerImageMultiplier = 1.5;
+  const centerImageShiftY = width / 24;
+  const imageHeight = level !== 0 ? outer - inner - imageMargin : (outer - inner) * centerImageMultiplier;
+  const imageWidth = level !== 0 ? outer - inner - imageMargin : (outer - inner) * centerImageMultiplier;
+
+  // Functions to calculate SVGs -----------------------------------------
   // Path Functions
   const getPathColor = (d, i) => {
     const team = d.data.team;
@@ -40,12 +52,33 @@ const RadialBracketPie = (props) => {
     return path(d);
   }
 
+  // Text Functions
+  const getTextTransform = (d, i, round) => {
+    const level = Math.log2(round);
+    let angle = ((d.startAngle + d.endAngle) / 2) * 180 / Math.PI;
+    if (angle > 90 && angle < 270) angle = angle + 180;
+    return level !== 0 ? `translate(${path.centroid(d)}) rotate(${angle})` : null;
+  }
+
+  const  getTextFontSize = () => {
+    return width / 18.75 + (3 * (5 - level));
+  };
+
+  // Image Functions
+  const getImageTransform = (d, i, round) => {
+    if (level === 0) {
+      const translation = path.centroid(d);
+      return `translate(${translation[0]}, ${translation[1] - centerImageShiftY})`;
+    }
+    return `translate(${path.centroid(d)})`;
+  }
+
   //Data Setup
   const arcs = pie().value(1)(round);
   const path = arc().outerRadius(outer).innerRadius(inner);
 
   const bracketPie = arcs.map((d, i, round) => {
-    //Path
+    //Path SVG
     const color = getPathColor(d, i);
     const path = getPiePath(d, i, round);
 
@@ -59,12 +92,45 @@ const RadialBracketPie = (props) => {
         ></path>
     );
 
+    // Text SVG
+    const textTransform = getTextTransform(d, i, round);
+    const textFontSize = getTextFontSize();
+
+    const textSvg = (
+      <text
+        key={`text__${level}--${i}`}
+        className={`RadialBracketPie__text`}
+        transform={textTransform}
+        dy={textY}
+        textAnchor={'middle'}
+        fontSize={textFontSize}
+        fill={'white'}
+      >
+        {d.data.team.name ? d.data.team.name : null }
+      </text>
+    );
+
+     // Image SVG
+     const imageTransform = getImageTransform(d, i, round)
+     const imageLink = d.data.team.name !== '' ? nbaLogos[d.data.team.name][d.data.team.logo] : '#';
+     const imageSvg = (
+       <image
+         key={`image__${level}--${i}`}
+         href={imageLink}
+         height={imageHeight}
+         width={imageWidth}
+         x={-imageWidth / 2}
+         y={-imageHeight / 2}
+         transform={imageTransform}
+       ></image>
+     );
+
     return (
       <g
         key={`group__${level}--${i}`}
         onClick={(e) => console.log('hello')}
       >
-        { [pathSvg] }
+        { showImages ? [pathSvg, imageSvg] : [pathSvg, textSvg] }
       </g>
   );
   })
