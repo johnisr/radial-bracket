c138be153e41df8989b90e542e18aa612d7fa7c3
John <john.i.s.ramos@gmail.com>
2018-04-25 20:19:17 -0400

    Change default of showing wins to false

--- src/components/RadialBracketPage/RadialBracketPage.js
+++ src/components/RadialBracketPage/RadialBracketPage.js
@@ -14,7 +14,7 @@ class RadialBracketPage extends React.Component {
   state = {
     dimensions: [600, 700],
     margin: { top: 100, right: 0, bottom: 0, left: 0 },
-    showWins: true,
+    showWins: false,
     showImages: false,
     titleFontFamily: 0,
     nameFontFamily: 0,
